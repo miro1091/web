ff98384bc05ee7da10ccd9bb0879ecd56192c989
mledecky1091 <marekmatej.firma@gmail.com>
2018-10-30 23:15:38 +0100

    first commit
    Signed-off-by: mledecky1091 <marekmatej.firma@gmail.com>

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -27,10 +27,10 @@ const Footer = props => {
         {t("if you found this useful")}
       </div>
       <div className="ml-auto">
-        <b className="hidden-xs-down">{t("Core Version")}: </b>
+        {/* <b className="hidden-xs-down">{t("Core Version")}: </b>
         <b className="hidden-sm-up">C: </b> <span id="piholeVersion">vDev</span>
         <b className="hidden-xs-down">, {t("Web Version")}: </b>
-        <b className="hidden-sm-up">, W: </b> <span id="webVersion">vDev</span>
+        <b className="hidden-sm-up">, W: </b> <span id="webVersion">vDev</span> */}
       </div>
     </footer>
   );
